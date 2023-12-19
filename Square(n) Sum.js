ce4f52391c9c665364116b27d772b5c5b34234ae
dustics4 <72154313+dustics4@users.noreply.github.com>
2023-12-19 16:07:29 +0000

    Create Square(n) Sum.js

---
+++ Square(n) Sum.js
@@ -0,0 +1,9 @@
+function squareSum(numbers){
+  let num = 0;
+  for ( i = 0 ; i < numbers.length; i++){
+    num += numbers[i] **2
+  }
+  return num
+}
+
+//For example, for [1, 2, 2] it should return 9  
