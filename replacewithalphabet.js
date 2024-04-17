2eb6ef7edcf89901a810df3e65f530a5dc4e869a
dustics4 <konrad.babowicz21@gmail.com>
2024-04-17 22:20:03 +0100

    resolution with comments

--- replacewithalphabet.js
+++ replacewithalphabet.js
@@ -1,14 +1,18 @@
 function replaceAlphabet(str){
-    let lowerText = str.toUpperCase();
+    //convert text to uppercase
 
-    let arr = lowerText.split("").map(let => let.charCodeAt());
+    let upperText = str.toUpperCase();
 
+    //map over each letter converting it to charCode
+    let arr = upperText.split("").map(let => let.charCodeAt());
+
+    //create a new array with the correct numerical values
     const newArr = arr.filter(num => {
         if(num > 64 && num < 91){
             return num
         }
     })
-
+    //create a third array where we will store the numbers. For each charcode num we -64 to provide the number of each letter correct
     const updatedNumsArr = newArr.map(num => num - 64);
 
     return updatedNumsArr.join(" ");
