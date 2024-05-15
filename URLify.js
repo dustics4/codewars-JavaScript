414671830d50338bca2637e4967a2a93d832f35b
dustics4 <konrad.babowicz21@gmail.com>
2024-05-15 22:16:45 +0100

    added comments

--- URLify.js
+++ URLify.js
@@ -2,10 +2,14 @@ function urlIfy(str){
     
     let char = "%20";
     let outputStr = '';
+    //loop through string
     for(let i = 0; i < str.length; i++){
+        //strin index is equal to empty space
         if(str[i] === " "){
+            //if yes, replace it with the characters sequence
             outputStr += char;
         }else{
+            //if not, append hte charcaters in to the output string
             outputStr += str[i];
         }
     }
