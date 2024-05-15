09f06b81a1e8a2e276a69b100aad482532bfcfa4
dustics4 <konrad.babowicz21@gmail.com>
2024-05-15 22:15:18 +0100

    code complete, without regex

--- URLify.js
+++ URLify.js
@@ -1,5 +1,15 @@
 function urlIfy(str){
-
+    
+    let char = "%20";
+    let outputStr = '';
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === " "){
+            outputStr += char;
+        }else{
+            outputStr += str[i];
+        }
+    }
+    console.log(outputStr);
 }
 
 urlIfy("This is John");
