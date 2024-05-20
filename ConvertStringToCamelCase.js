b268471240797ca200f92b06c2412655dc8a42e1
dustics4 <konrad.babowicz21@gmail.com>
2024-05-20 20:14:00 +0100

    added comments

--- ConvertStringToCamelCase.js
+++ ConvertStringToCamelCase.js
@@ -1,12 +1,19 @@
 function toCamelCase(str){
 
+    //split the string first
     str = str.split('');
+    //use the map function to iterate over the characters
     console.log(str.map(function(el, i){
+    //Check if character is the same as - or _
       if(el == '-' || el == '_'){
+        //If it is, the character in from of the elemnt is changed to uppercase
         el = str[i+1].toUpperCase();
+        //once that is done, we remove the character
         str.splice(i+1, 1);
       }
+      //return the element
       return el;
+    //lastly join the string.
     }).join(''));
 }
 
