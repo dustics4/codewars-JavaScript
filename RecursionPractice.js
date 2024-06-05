b8beae986ba5cf083f4a028b2da537dad89b05c0
dustics4 <konrad.babowicz21@gmail.com>
2024-06-05 22:41:44 +0100

    3 possible solutions

---
+++ RecursionPractice.js
@@ -0,0 +1,26 @@
+const sumTo = (n) => {
+    //using a for loop
+    let sum = 0;
+
+    for(let i = 1; i <= n; i++){
+        sum += i;
+    }
+    console.log(sum);
+}
+
+
+function sumTo2(n){
+    //using a recursion 
+    if(n == 1) return 1;
+    return n + sumTo2(n - 1); 
+}
+
+function sumTo3(n){
+    //using the arithmetic progression formula
+    return n * (n + 1) / 2;
+}
+
+
+sumTo(3);
+sumTo2(3);
+sumTo3(3);
