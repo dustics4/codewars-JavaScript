e46b77875f8007375dd7538504e7ebfe68c9ece5
dustics4 <konrad.babowicz21@gmail.com>
2024-06-10 22:36:40 +0100

    sum of array integer

--- recursionExercises.js
+++ recursionExercises.js
@@ -37,3 +37,21 @@ function all(array, callback){
         return false;
     }
 }
+
+var seven = totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
+console.log(seven);
+
+function totalIntegers(array){
+	if(array.length === 0) return 0;
+
+	let total = 0;
+	let first = array.shift();
+
+	if (Array.isArray(first)){
+		total += totalIntegers(first); 
+	} else if (Number.isInteger(first)) {
+		total += 1;
+	}
+
+	return total + totalIntegers(array);
+}
